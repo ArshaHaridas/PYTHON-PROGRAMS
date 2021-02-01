73f6a0c7bf683e63e3811b9eecaa7f1b03a3386f
ArshaHaridas <77680795+ArshaHaridas@users.noreply.github.com>
2021-02-01 11:08:33 +0530

    initial commit
    Write a program to demonstrate ZeroDivisionError and ValueError

---
+++ exception1.py
@@ -0,0 +1,10 @@
+try:
+	n1=int(input("enter 1st number:"))
+	n2=int(input("enter 2nd number:"))
+	print("quotient=",(n1/n2))
+except(ZeroDivisionError,ValueError) as e:
+	print(e)
+else:
+	print("Divided Successfully")
+finally:
+	print("Finally block")
